da2725da34b7c7f6ae8745295048a2911752d51e
moriharanariki <nariki@gmail.com>
2023-01-04 16:36:18 +0900

    商品一覧表示機能の実装と商品検索機能の実装を行なった。

--- src/main/java/com/example/demo/service/showItemListService.java
+++ src/main/java/com/example/demo/service/showItemListService.java
@@ -3,8 +3,11 @@ package com.example.demo.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.example.demo.domain.Category;
 import com.example.demo.domain.Item;
+import com.example.demo.repository.CategoryRepository;
 import com.example.demo.repository.ItemRepository;
 
 /**
@@ -13,10 +16,14 @@ import com.example.demo.repository.ItemRepository;
  * @author moriharanariki
  *
  */
-public class showItemListService {
+@Service
+public class ShowItemListService {
 	
 	@Autowired
 	private ItemRepository itemRepository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 	
 	
 	/**
@@ -30,6 +37,57 @@ public class showItemListService {
 	public List<Item> showItemList(String name, Integer category, String brand){
 		List<Item> itemList = itemRepository.findByNameAndCategoryAndBrand(name, category, brand);
 		return itemList;
+	}
+	
+	/**
+	 * 大カテゴリの情報を取得します.
+	 * 
+	 * @return
+	 */
+	public List<Category> showLargeCategoryList(){
+		List<Category> largeCategoryList = categoryRepository.findByLargeCategory();
+		return largeCategoryList;
+	}
+	
+	/**
+	 * 中カテゴリの情報を取得します.
+	 * 
+	 * @return
+	 */
+	public List<Category> showMediumCategoryList(){
+		List<Category> mediumCategoryList = categoryRepository.findByMediumCategory();
+		return mediumCategoryList;
+	}
+	
+	/**
+	 * 親IDで検索した中カテゴリ情報を取得します.
+	 * 
+	 * @param id
+	 * @return
+	 */
+	public List<Category> showParentIdMediumCategoryList(Integer id){
+		List<Category> mediumCategoryList = categoryRepository.findByParentIdMediumCategory(id);
+		return  mediumCategoryList;
+	}
+	
+	/**
+	 * 小カテゴリの情報を取得します.
+	 * 
+	 * @return
+	 */
+	public List<Category> showSmallCategoryList(){
+		List<Category> smallCategoryList = categoryRepository.findBySmallCategory();
+		return smallCategoryList;
+	}
+	
+	/**
+	 * 小カテゴリの情報を取得します.
+	 * 
+	 * @return
+	 */
+	public List<Category> showParentIdSmallCategoryList(Integer parentId){
+		List<Category> smallCategoryList = categoryRepository.findByParentIdSmallCategory(parentId);
+		return smallCategoryList;
 	}
 	
 }
